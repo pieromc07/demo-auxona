6b7325a55549b329e74742c7f9d992bf8856d1e1
Piero Merino Carpio <piero0716.mc@gmail.com>
2023-02-25 12:12:25 -0500

    :construction: manejo de controles y diseño del historial

--- src/public/main.js
+++ src/public/main.js
@@ -1,6 +1,7 @@
 let player;
 let playerState = 0;
-let dateInit;
+let history = [];
+let historyIndex = 0;
 
 //* Create an <iframe> (and YouTube player)
 function onYouTubeIframeAPIReady() {
@@ -22,6 +23,7 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady(event) {
 	console.log("Cargo el video");
+	setInterval(updateTime, 1000);
 }
 
 var done = false;
@@ -35,6 +37,16 @@ function youTubePlayerChangeVideoId(videoId) {
 	player.cueVideoById({ suggestedQuality: "medium", videoId: videoId });
 	player.pauseVideo();
 	playerState = 0;
+}
+
+function updateTime() {
+	var currentTime = player.getCurrentTime();
+	var duration = player.getDuration();
+	currentTime = Math.floor(currentTime);
+	const player_current = document.querySelector('#player-current');
+	player_current.innerHTML = getDurationString(currentTime);
+	const progress_track = document.querySelector('#progress-track');
+	progress_track.value = (currentTime / duration) * 100;
 }
 
 async function play(id) {
@@ -69,16 +81,21 @@ async function play(id) {
 	player_current.innerHTML = '00:00';
 	progress_volume.value = player.getVolume();
 	progress_track.value = player.getCurrentTime();
-
-
+	if (progress_volume.value == 0) {
+		document.querySelector('#icon-volume').classList.remove('fa-volume-up');
+		document.querySelector('#icon-volume').classList.add('fa-volume-mute');
+	} else {
+		document.querySelector('#icon-volume').classList.remove('fa-volume-mute');
+		document.querySelector('#icon-volume').classList.add('fa-volume-up');
+	}
 	youTubePlayerChangeVideoId(track.youtube_id);
 	if (playerState == 0) {
 		//* Espera 500ms para que cargue el video
 		setTimeout(() => {
 			player.playVideo();
 			playerState = 1;
-			dateInit = new Date();
-			activarEvento();
+			addHistory(track);
+			updateHistory();
 			if (saved) {
 				saveTrack(track);
 			}
@@ -135,10 +152,76 @@ function getDurationString(duration) {
 	return minutes + ':' + seconds;
 }
 
+function addHistory(track) {
+	//* Agregar a la lista de reproduccion si no existe
+	if (history.length == 0) {
+		history.push(track);
+	} else {
+		let exists = false;
+		history.map((item, index) => {
+			if (item.deezer_id == track.deezer_id) {
+				exists = true;
+				history.splice(index, 1);
+				history.push(track);
+			}
+		}
+		);
+		if (!exists) {
+			history.push(track);
+		}
+	}
+}
+function updateHistory() {
+	const content = document.querySelector('#history');
+	if (history.length > 0) {
+		content.innerHTML = '';
+		history.map(track => {
+			const history_content = document.createElement('div');
+			history_content.classList.add('history_content');
+			const history_content_item = document.createElement('div');
+			history_content_item.classList.add('history_content_item');
+			const history_content_item_image = document.createElement('div');
+			history_content_item_image.classList.add('history_content_item_image');
+			const history_content_item_image_img = document.createElement('img');
+			history_content_item_image_img.src = track.md5_image;
+			const history_content_item_image_play = document.createElement('button');
+			history_content_item_image_play.classList.add('history_content_item_image_play');
+			history_content_item_image_play.addEventListener('click', () => {
+				play(track.deezer_id);
+			}
+			);
+			const history_content_item_image_play_icon = document.createElement('i');
+			history_content_item_image_play_icon.classList.add('fas');
+			history_content_item_image_play_icon.classList.add('fa-play');
+			const history_content_item_description = document.createElement('div');
+			history_content_item_description.classList.add('history_content_item_description');
+			const history_content_item_description_title = document.createElement('p');
+			history_content_item_description_title.classList.add('history_content_item_description_title');
+			history_content_item_description_title.innerHTML = track.title;
+			const history_content_item_description_artist = document.createElement('p');
+			history_content_item_description_artist.classList.add('history_content_item_description_artist');
+			history_content_item_description_artist.innerHTML = track.artist_name;
+
+			history_content_item_image_play.appendChild(history_content_item_image_play_icon);
+			history_content_item_image.appendChild(history_content_item_image_img);
+			history_content_item_image.appendChild(history_content_item_image_play);
+			history_content_item_description.appendChild(history_content_item_description_title);
+			history_content_item_description.appendChild(history_content_item_description_artist);
+			history_content_item.appendChild(history_content_item_image);
+			history_content_item.appendChild(history_content_item_description);
+			history_content.appendChild(history_content_item);
+			content.appendChild(history_content);
+		});
+	} else {
+		content.innerHTML = '<div class="history-empty">No hay historial</div>';
+	}
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
-	const search = document.querySelector('#search');
+	updateHistory();
 
+	const search = document.querySelector('#search');
 	search.addEventListener('click', async () => {
 		const query = document.querySelector('#data-search').value;
 		if (query) {
@@ -199,24 +282,33 @@ document.addEventListener('DOMContentLoaded', () => {
 			player.pauseVideo();
 			playerState = 0;
 		}
+		if (playerState == 0) {
+			document.querySelector('#icon-play').classList.remove('fa-play');
+			document.querySelector('#icon-play').classList.add('fa-pause');
+		} else {
+			document.querySelector('#icon-play').classList.remove('fa-pause');
+			document.querySelector('#icon-play').classList.add('fa-play');
+		}
+
 	});
 
 	const progress_track = document.querySelector('#progress-track');
 	progress_track.addEventListener('change', (e) => {
-		console.log('change');
 		player.currentTimeSliding = false;
 		player.seekTo(e.target.value * player.getDuration() / 100, true);
-		// volver activar el evento change
-		activarEvento()
-	});
-
-	progress_track.addEventListener('oninput', (e) => {
-		player.currentTimeSliding = true;
 	});
 
 	const progress_volume = document.querySelector('#progress-volume');
+	const icon_volumen = document.querySelector('#icon-volume');
 	progress_volume.addEventListener('change', (e) => {
 		player.setVolume(e.target.value);
+		if (e.target.value == 0) {
+			icon_volumen.classList.remove('fa-volume-up');
+			icon_volumen.classList.add('fa-volume-mute');
+		} else {
+			icon_volumen.classList.remove('fa-volume-mute');
+			icon_volumen.classList.add('fa-volume-up');
+		}
 	});
 
 	const player_volume = document.querySelector('#player-volume');
@@ -228,33 +320,26 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
+	const player_next = document.querySelector('#player-next');
+	player_next.addEventListener('click', () => {
+		if(historyIndex < history.length - 1){
+			historyIndex++;
+			play(history[historyIndex].deezer_id);
+		}else{
+			alert('No hay mas canciones');
+		}
+	});
+
+	const player_back = document.querySelector('#player-back');
+	player_back.addEventListener('click', () => {
+		if(historyIndex > 0){
+			historyIndex--;
+			play(history[historyIndex].deezer_id);
+		}else{
+			alert('No hay mas canciones');
+		}
+	});
 });
-function activarEvento() {
-	const progress_track = document.querySelector('#progress-track');
-	let mode = 0;
-	// si existe el evento change lo elimina
-	if (progress_track.onchange) {
-		progress_track.onchange = null;
-		mode = 1;
-		console.log('eliminado');
-	}
-	// activa el evento change
-	progress_track.onchange = changeTime(mode);
-	console.log('activado');
-}	
 
-function changeTime(mode) {
-	const player_current = document.querySelector('#player-current');
-	const progress_track = document.querySelector('#progress-track');
 
-	if (mode == 0) {
-		var currentDate = new Date();
-		var seconds = (currentDate - dateInit) / 1000;
-		progress_track.value = seconds;
-		console.log('change ' + seconds);
-	}else{
-		progress_track.value = player.getCurrentTime() * 100 / player.getDuration();
-		console.log('change 1');
-	}
-	// activarEvento();
-}
+
