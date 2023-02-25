e57cf93628678022db988e0618eac1f03bb53606
Piero Merino Carpio <piero0716.mc@gmail.com>
2023-02-25 13:16:16 -0500

    🚀 file vercel deploy

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,13 @@ import app from "./app/app.routes.js";
 const router = Router();
 
 router.use('/app', app);
+router.get('/', (req, res) => {
+    res.redirect('/app');
+});
+router.get('*', (req, res) => {
+    res.redirect('/app');
+});
+
 
 
 
